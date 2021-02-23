4c81c75e0401a7ca8f57ab33c7b5b766022e9f3a
xiongbin <923450980@qq.com>
2021-02-23 17:19:06 +0800

    first commit

---
+++ autowatering.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		autowatering.ino
+ Created:	2021/2/23 17:07:59
+ Author:	Administrator
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
